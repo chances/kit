5cb4d298fec623b26a6d4a9d38b364049396932d
Chance Snow <git@chancesnow.me>
2021-12-28 21:02:41 -0600

    Refactor position to span conversions

--- src/bootstrap/ast/span.h
+++ src/bootstrap/ast/span.h
@@ -19,3 +19,27 @@ typedef struct Span Span;
 Span NoPos = { NilString, { 0, 0 }, { 0, 0 } };
 
 Maybe(Span);
+
+Position posToPosition(String source, int pos) {
+  Position position;
+
+  int line = 1;
+  int col = 0;
+  for (int i = 0; i <= pos; i += 1) {
+    if (source.cString[i] == '\n') {
+      line++;
+      col = 0;
+    }
+    else col += 1;
+  }
+  position.line = line;
+  position.column = col;
+
+  return position;
+}
+
+Span posToSpan(String source, int startPos, int endPos) {
+  Position start = posToPosition(source, startPos);
+  Position end = posToPosition(source, endPos);
+  return (Span) { source, start, end };
+}
--- src/bootstrap/document.h
+++ src/bootstrap/document.h
@@ -9,27 +9,3 @@ struct Document {
   size_t lineLength;
 };
 typedef struct Document Document;
-
-Position posToPosition(Document* document, int pos) {
-  Position position;
-
-  int line = 0;
-  int col = 0;
-  for (int i = 0; i <= pos; i += 1) {
-    if (document->contents.cString[i] == '\n') {
-      line++;
-      col = 0;
-    }
-    else col += 1;
-  }
-  position.line = line;
-  position.column = col;
-
-  return position;
-}
-
-Span posToSpan(Document* document, int startPos, int endPos) {
-  Position start = posToPosition(document, startPos);
-  Position end = posToPosition(document, endPos);
-  return (Span) { document->file, start, end };
-}
